5e5923b90c77ee7b4b5c19a3f3dad4aae10d9c2a
icyleaf <icyleaf.cn@gmail.com>
2018-04-10 17:08:22 +0800

    feat(logger): add progname, formatter params

--- src/salt/middlewares/logger.cr
+++ src/salt/middlewares/logger.cr
@@ -6,12 +6,16 @@ module Salt::Middlewares
   # Salt.use Salt::Middlewares::Logger, level: Logger::ERROR
   # ```
   class Logger < App
-    def initialize(@app : App, @io : IO = STDOUT, @level : ::Logger::Severity = ::Logger::INFO)
+    def initialize(@app : App, @io : IO = STDOUT, @progname = "salt",
+                   @level : ::Logger::Severity = ::Logger::INFO,
+                   @formatter : ::Logger::Formatter? = nil)
     end
 
     def call(env)
       logger = ::Logger.new(@io)
       logger.level = @level
+      logger.progname = @progname
+      logger.formatter = @formatter.not_nil! if @formatter
 
       env.logger = logger
       call_app(env)
