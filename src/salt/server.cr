41db28e7fa23fd976815949e43f9ec653b55394f
icyleaf <icyleaf.cn@gmail.com>
2018-04-11 10:15:47 +0800

    doc: update README

--- src/salt/server.cr
+++ src/salt/server.cr
@@ -64,7 +64,7 @@ module Salt
     end
 
     private def build_app(app : Salt::App) : Salt::App
-      if middwares = default_middlewares[@options["environment"]]?
+      if middlewares = default_middlewares[@options["environment"]]?
         middlewares.each { |klass| app = klass.new(app) }
       end
 
