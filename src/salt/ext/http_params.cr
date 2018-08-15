9afb06dfd7145d61801b734bd447210b338f6c4d
icyleaf <icyleaf.cn@gmail.com>
2018-08-15 10:23:58 +0800

    release: dump v0.4.2

--- src/salt/ext/http_params.cr
+++ src/salt/ext/http_params.cr
@@ -1,4 +1,4 @@
-
+{% if Crystal::VERSION < "0.26.0" %}
   # :nodoc:
   # PR was merged and included to v0.26.0: https://github.com/crystal-lang/crystal/pull/6241
   module HTTP
--- src/salt/version.cr
+++ src/salt/version.cr
@@ -1,3 +1,3 @@
 module Salt
-  VERSION = "0.4.1"
+  VERSION = "0.4.2"
 end
